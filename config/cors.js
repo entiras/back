aaa0958cc87ed76acf80f3690fc1657414f938ba
operezcham <operezcham@gmail.com>
2020-04-30 00:32:05 -0500

    cors

--- config/cors.js
+++ config/cors.js
@@ -16,7 +16,7 @@ module.exports = {
   | Function - Receives the current origin and should return one of the above values.
   |
   */
-  origin: 'entira.tk,entiras.herokuapp.com',
+  origin: ['entira.tk', 'entiras.herokuapp.com'],
 
   /*
   |--------------------------------------------------------------------------
