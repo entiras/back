f64e12d6bbdf842ec5be81072a05cb23f6f20289
operezcham <operezcham@gmail.com>
2020-05-08 01:43:26 -0500

    git

--- app/Controllers/Http/PageController.js
+++ app/Controllers/Http/PageController.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const Env = use('Env')
+const Env = use('Env');
 const { Octokit } = require("@octokit/rest");
 const octokit = new Octokit({
   auth: Env.get('GITHUB_TOKEN', '')
@@ -10,6 +10,13 @@ class PageController {
   async home({ response }) {
     await octokit.repos.createForAuthenticatedUser({
       name: 'Hola'
+    });
+    await octokit.repos.createOrUpdateFile({
+      owner: 'entiras',
+      repo: 'fron',
+      path: 'hola',
+      message: 'hola',
+      content: 'SG9sYQ=='
     });
     return response.json({
       status: '✔️'
