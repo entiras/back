f9706a842d34baa5596440aa0c00f104d0b806eb
operezcham <operezcham@gmail.com>
2020-05-10 22:21:36 -0500

    mongo

--- app/Controllers/Http/PageController.js
+++ app/Controllers/Http/PageController.js
@@ -6,8 +6,9 @@ const octokit = new Octokit({
   auth: Env.get('GITHUB_TOKEN', '')
 });
 const MongoClient = require('mongodb').MongoClient;
-const mongo = new MongoClient(Env.get('MONGO_URI', ''), { useNewUrlParser: true });
-mongo.connect();
+const mongo = new MongoClient(Env.get('MONGO_URI', ''), {
+  useNewUrlParser: true
+});
 
 class PageController {
   async home({ response }) {
@@ -27,12 +28,14 @@ class PageController {
   }
   async script({ response, view }) {
     // list files
+    mongo.connect();
     const files = await mongo.db('entiras').collection('files').find({});
     const arr = [];
     var f = null;
     while (f = await files.next()) {
       arr.push(f);
     }
+    mongo.close();
     return response.json({
       files: arr
     });
