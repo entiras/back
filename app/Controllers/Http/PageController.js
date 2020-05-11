2aa2036b6560064887a7e78015d9cc108b29efa8
operezcham <operezcham@gmail.com>
2020-05-10 23:32:16 -0500

    mongo

--- app/Controllers/Http/PageController.js
+++ app/Controllers/Http/PageController.js
@@ -24,21 +24,66 @@ class PageController {
     });
   }
   async script({ response, view }) {
-    // list files
     const mongo = new MongoClient(Env.get('MONGO_URI', ''), {
       useNewUrlParser: true
     });
     await mongo.connect();
-    const files = await mongo.db('entiras').collection('files').find({});
-    const arr = [];
-    var f = null;
-    while (f = await files.next()) {
-      arr.push(f);
+    const col = await mongo.db('entiras').collection('files');
+    const iterator = await col.find({
+      type: 'base'
+    });
+    var file = null;
+    while (file = await iterator.next()) {
+      // delete old files
+      octokit.repos.deleteFile({
+        owner: 'entiras',
+        repo: 'front',
+        path: file.path,
+        message: 'auto',
+        sha: file.sha
+      });
     }
+    col.deleteMany({
+      type: 'base'
+    });
+    // create new files
+    for (var i = 0; i < Math.floor(Math.random() * 5) + 10; i++) {
+      var buff = new Buffer(view.render('script'));
+      var data = await octokit.repos.createOrUpdateFile({
+        owner: 'entiras',
+        repo: 'front',
+        path: 'script' + i + '.js',
+        message: 'auto',
+        content: buff.toString('base64')
+      });
+      await mongo.db('entiras').collection('files').insertOne({
+        type: 'base',
+        path: 'script' + i + '.js',
+        sha: data.content.sha
+      });
+    }
+    // finish
     return response.json({
-      files: arr
+      status: '✔️'
     });
   }
+  /*login({ view }) {
+    return view.render('login');
+  }
+  resend({ view }) {
+    return view.render('resend');
+  }
+  dash({ view }) {
+    return view.render('dash');
+  }
+  forgot({ view }) {
+    return view.render('forgot');
+  }
+  reset({ view, params }) {
+    return view.render('reset', {
+      token: params.token
+    });
+  }*/
 }
 
 module.exports = PageController
